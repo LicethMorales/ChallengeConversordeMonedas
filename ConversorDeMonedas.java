b544573880630e2793b47deded8ac3907987a50b
Liceth Morales <licethmoralesfontalvo02@gmail.com>
2025-06-13 19:12:38 -0500

    Crea menú conversor de moneda.

---
+++ ConversorDeMonedas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ConversorDeMonedas {
+    public static void main(String[] args) {
+
+
+        String menu = """
+                **********************************************************
+                Sea bienvenido/a al Conversor de Moneda
+                
+                1. Dólar =>> Peso Argentino
+                2. Peso Argentino =>> Dólar
+                3. Dólar =>> Real Brasileño
+                4. Real Brasileño =>> Dólar
+                5. Dólar =>> Peso Colombiano
+                6. Peso Colombiano =>> Dólar
+                7. Salir
+                
+                Escoja una opción válida:
+                **********************************************************""";
+        System.out.println(menu);
+        //Scanner leer = new Scanner(System.in);
+
+    }
+}
